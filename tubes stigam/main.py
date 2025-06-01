c70f881c10b4407cc4f5eef8e8d9fce0721e0ff9
Muharyan12 <ryryry120200@gmail.com>
2025-06-01 14:25:19 +0700

    menghapus bot selain stigam

--- tubes stigam/main.py
+++ tubes stigam/main.py
@@ -16,9 +16,7 @@ init()
 BASE_URL = "http://localhost:3000/api"
 DEFAULT_BOARD_ID = 1
 CONTROLLERS = {
-    "Stigam": Stigam, 
-    "Awi": MyBot,
-    "NotUnderstand": NotUnderstand
+    "Stigam": Stigam
 }
 
 ###############################################################################
